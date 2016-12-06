074ef69eefc438e3330dfb0a656a2e6ed8a8f254
cranapplejam <jean.ann.meier@emory.edu>
2016-12-06 15:04:04 -0500

    add files via upload
    included original hypo (not mine) plus archived and published versions

---
+++ Hypo.app/Contents/hypo-mac/hypo-mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  hypo-mac
+//
+//  Created by sts on 6/9/16.
+//  Copyright © 2016 sts. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hypo.app/Contents/hypo-mac/hypo-mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  hypo-mac
+//
+//  Created by sts on 6/9/16.
+//  Copyright © 2016 sts. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
